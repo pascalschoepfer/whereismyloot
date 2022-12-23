e989910337284145df088cba88e4aeee0135dd0b
pascalschoepfer <pascal@schoepfer.xyz>
2022-12-23 08:35:11 +0100

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css'
 import {useEffect} from "react";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   useEffect(()=>{
     import("bootstrap/dist/js/bootstrap");
   },[])
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Component {...pageProps} />
+        <Analytics />
+      </>
+      )
 }
 
 export default MyApp
